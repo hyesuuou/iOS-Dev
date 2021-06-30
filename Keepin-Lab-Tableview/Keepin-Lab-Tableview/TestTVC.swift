f7cd5fc206c691237c61fb475f647e1ba45c7099
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-06-30 19:52:13 +0900

    [Prac] 선택된 사람 위 section으로 올리기

--- Keepin-Lab-Tableview/Keepin-Lab-Tableview/TestTVC.swift
+++ Keepin-Lab-Tableview/Keepin-Lab-Tableview/TestTVC.swift
@@ -12,9 +12,14 @@ class TestTVC: UITableViewCell {
     public static let identifier = "TestTVC"
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var selectedLabel: UILabel!
+    
+    //var clickState : Bool = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        selectedLabel.text = "X"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -23,8 +28,24 @@ class TestTVC: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    func setData(name : String) {
+    func setData(name : String, clicked : Bool) {
         nameLabel.text = name
+        if clicked == true {
+            selectedLabel.text = "O"
+        }
+        else {
+            selectedLabel.text = "X"
+        }
+       
     }
+    
+//    @IBAction func selectedButtonClicked(_ sender: Any) {
+//        if (clickState == true) {
+//            clickState = false
+//        }
+//        else {
+//            clickState = true
+//        }
+//    }
     
 }
--- Keepin-Lab-Tableview/Keepin-Lab-Tableview/ViewController.swift
+++ Keepin-Lab-Tableview/Keepin-Lab-Tableview/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     let data : [String] = ["김혜수", "최이준", "이채연", "박윤정", "박윤경", "이은영", "김영민", "손연주", "김보민"]
-    var filteredData : [String]!
+    var filteredData : [String] = [] // 검색된 결과
+    var seletedData : [String] = [] // 선택된 데이터
     
     @IBOutlet weak var searchbar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
@@ -41,7 +42,7 @@ extension ViewController : UISearchBarDelegate {
         filteredData = []
         
         if searchText == "" {
-            filteredData = data
+            //filteredData = data
         }
         else {
             for name in data {
@@ -59,16 +60,62 @@ extension ViewController : UITableViewDelegate {
 }
 
 extension ViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredData.count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2  // index 0: 선택된 친구 / index 1: 검색된 친구
     }
+    
+    // 각 Section 안에 rows가 몇개?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return seletedData.count
+        case 1:
+            return filteredData.count
+        default:
+            return 0
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let cell = tableview.dequeueReusableCell(withIdentifier: TestTVC.identifier, for: indexPath) as? TestTVC
-        else { return UITableViewCell()}
-        cell.setData(name: filteredData[indexPath.row])
-        return cell
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableview.dequeueReusableCell(withIdentifier: TestTVC.identifier, for: indexPath) as? TestTVC
+            else { return UITableViewCell()}
+            cell.setData(name: seletedData[indexPath.row], clicked: true)
+            return cell
+            
+        case 1:
+            guard let cell = tableview.dequeueReusableCell(withIdentifier: TestTVC.identifier, for: indexPath) as? TestTVC
+            else { return UITableViewCell()}
+            cell.setData(name: filteredData[indexPath.row], clicked: false)
+            return cell
+        default:
+            return UITableViewCell()
+        }
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            print("먼데..")
+            // 여기서 선택하면 해당 내용 filteredData에 추가 후 내용 삭제
+            filteredData.append(seletedData[indexPath.row])
+            seletedData.remove(at: indexPath.row)
+            
+        case 1:
+            // 여기서 선택하면 해당 내용 삭제 후 section 0 (selectedData에 추가)
+            seletedData.append(filteredData[indexPath.row])
+            filteredData.remove(at: indexPath.row)
+            
+        default:
+            print("선택")
+        }
+        tableview.reloadData()
     }
     
     
