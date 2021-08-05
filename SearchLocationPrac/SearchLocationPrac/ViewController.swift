6cd80595ee47aa30441c0bcbfe0046ebc0f6ad07
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-08-05 16:13:01 +0900

    [#2] 가져온 주소 정보 출력

--- SearchLocationPrac/SearchLocationPrac/ViewController.swift
+++ SearchLocationPrac/SearchLocationPrac/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchbar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
     
-    
+    var delegate: UISearchBarDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,6 +55,27 @@ extension ViewController: MKLocalSearchCompleterDelegate {
 }
 
 extension ViewController: UITableViewDelegate {
+    
+    // 선택된 위치의 정보 가져오기
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedResult = searchResults[indexPath.row]
+        let searchRequest = MKLocalSearch.Request(completion: selectedResult)
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            guard error == nil else {
+                return
+            }
+            guard let placeMark = response?.mapItems[0].placemark else {
+                return
+            }
+            
+            print("가져온 주소", placeMark.coordinate)
+            print("나라이름", placeMark.countryCode!)
+            print("지역이름", placeMark.title!)
+        }
+        
+    }
     
 }
 
