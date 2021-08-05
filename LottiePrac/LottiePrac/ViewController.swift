3d74453a82312e13ee3a2ea52b4a56493c3f7642
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-08-06 05:35:24 +0900

    [#5] 화면에 로티뷰 넣기 연습

---
+++ LottiePrac/LottiePrac/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LottiePrac
+//
+//  Created by 김혜수 on 2021/08/06.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    lazy var lottieView : AnimationView = {
+        let animationView = AnimationView(name: "4800-weather-partly-cloudy")
+        animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        animationView.play()
+        animationView.isHidden = false
+        return animationView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(lottieView)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LottiePrac/Pods/Target Support Files/Pods-LottiePrac/Pods-LottiePrac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LottiePrac : NSObject
+@end
+@implementation PodsDummy_Pods_LottiePrac
+@end
